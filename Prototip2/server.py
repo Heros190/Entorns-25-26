d45ec72b9a08d6945351c25de99619d9b0303b07
Llorenc <llorens.abad@gmail.com>
2026-03-04 12:27:12 +0100

    a

--- Prototip2/server.py
+++ Prototip2/server.py
@@ -51,3 +51,23 @@ def login():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+    
+@app.route('/child', methods=['POST'])
+def child():
+    data = request.get_json()
+    user_id = data.get('id_user') #id_user
+    response = ApiResponse(
+            msg="Child",
+            coderesponse="-1",
+            data=""
+        )
+    if (not user_id and not user_id.is_integer): #validacio de dades entrada
+        return jsonify(asdict(response)),400
+
+    user_id = int(user_id)
+    u=User(id=user_id, username="",passwdord="", email="", idrole=1, token="")
+    listChilds=ChilDao.getChilds(u)
+    response.coderesponse="1"
+    response.msg=len(listChilds)
+    response.data=listChilds
+    return jsonify(asdict(response)),200
