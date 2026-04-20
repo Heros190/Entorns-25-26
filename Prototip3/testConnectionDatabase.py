afd49a309a3c2cbbfa57681d07c00e47bb179dee
Llorenc <llorens.abad@gmail.com>
2026-04-20 14:46:45 +0200

    login + hash

--- Prototip3/testConnectionDatabase.py
+++ Prototip3/testConnectionDatabase.py
@@ -1,27 +1,70 @@
+from dataclasses import dataclass, asdict
+import hashlib
+from flask import jsonify
 import mysql.connector
 
-def list_users():
-    """
-    Connects to the MySQL database and lists all rows in the User table.
-    """
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="tapatapp"
-    )
-    cursor = connection.cursor(dictionary=True)
 
-    try:
-        query = "SELECT * FROM User"
-        cursor.execute(query)
-        users = cursor.fetchall()
+class UserDAO:
+    def connectBBDD(self):
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root",
+            database="tapatapp"
+        )
+        return connection
 
-        for user in users:
-            print(user)
-    finally:
+    def login(self, identifier, password):
+        #connect to database
+        con=self.connectBBDD()
+        cursor=con.cursor(dictionary=True)
+        query = """
+        SELECT * FROM User
+        WHERE (username = %s OR email = %s)
+        """
+        cursor.execute(query,(identifier, password))
+        user=cursor.fetchone()
         cursor.close()
-        connection.close()
+        con.close()
+        return user
+    
 
-if __name__ == "__main__":
-    list_users()
+
+    def setTokenUser(self, username):
+        #connect to database
+        con=self.connectBBDD()
+        cursor = con.cursor(dictionary=True)
+        # generate token
+        # update a BBDD camp token with the generated token
+        # close connection
+        cursor.close()
+        con.close()
+
+        def getHash(self, username):
+            miliseconds = str(time()*1000)
+            hash_object = hashlib.sha256(data.encode('utf-8'))
+            return hash_object.hexdigest()
+
+
+
+
+dao=UserDAO() 
+u=dao.login("mare","mare")
+print(u)
+
+u=dao.login("dasdad","mare")
+print(u)
+
+
+
+from time import time
+miliseconds = str(time()*1000)
+print("Time in miliseconds since epoch:", miliseconds)
+data = "Hola mundo "+miliseconds
+print(data)
+
+# 2. Crear el objeto hash SHA-256 y actualizarlo con los datos
+hash_object = hashlib.sha256(data.encode('utf-8'))
+#3. Obtener el resultado en formato hexadecimal
+token = hash_object.hexdigest()
+print(token)
