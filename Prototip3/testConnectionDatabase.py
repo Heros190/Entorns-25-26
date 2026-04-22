aa2ec216418470aa97f5bcd96ee57b1c08f04e43
Llorenc <llorens.abad@gmail.com>
2026-04-22 12:38:27 +0200

    a

--- Prototip3/testConnectionDatabase.py
+++ Prototip3/testConnectionDatabase.py
@@ -20,17 +20,17 @@ class UserDAO:
         con=self.connectBBDD()
         cursor=con.cursor(dictionary=True)
         query = """
-        SELECT * FROM User
-        WHERE (username = %s OR email = %s)
+            SELECT * FROM User
+            WHERE (username = %s OR email = %s)
         """
         cursor.execute(query,(identifier, password))
         user=cursor.fetchone()
+        if user:
+            self.setTokenUser(user["username"])
         cursor.close()
         con.close()
         return user
     
-
-
     def setTokenUser(self, username):
         #connect to database
         con=self.connectBBDD()
@@ -38,6 +38,7 @@ class UserDAO:
         # generate token
         token = self.getHash(username)
         # update a BBDD camp token with the generated token
+        print(type(token))
         query = "UPDATE User SET token ='"  + token + "' WHERE username = '" + username
         print(query)
         cursor.execute(query)
@@ -49,14 +50,14 @@ class UserDAO:
         miliseconds = str(time() * 1000)
         data = username + miliseconds
         hash_object = hashlib.sha256(data.encode('utf-8'))
-        return hash_object.hexdigest()
+        return hash_object.hexdigest() + ""
 
-"""     def getHash(self, username):
+    def getHash2(self, username):
         miliseconds = str(time() * random.randrange(100000))
         data = miliseconds
         hash_object = hashlib.sha256(data.encode('utf-8'))
-        return hash_object.hexdigest()
- """
+        return hash_object.hexdigest() + ""
+ 
 
 
 dao=UserDAO() 
@@ -71,7 +72,7 @@ print(u)
 miliseconds = str(time()*1000)
 print("Time in miliseconds since epoch:", miliseconds)
 data = "Hola mundo "+miliseconds
-print(data)
+print(data) 
 
 # 2. Crear el objeto hash SHA-256 y actualizarlo con los datos
 hash_object = hashlib.sha256(data.encode('utf-8'))
